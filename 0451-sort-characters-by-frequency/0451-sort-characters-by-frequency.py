bc4b05758b4389c9631392735026bcf97782a8aa
Kothapelly Sai Sidhartha <115478673+kothapallysidhartha@users.noreply.github.com>
2024-06-26 17:12:25 +0530

    Time: 29 ms (75.35%), Space: 13.9 MB (79.88%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def frequencySort(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        d={}
+        for i in s:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+        s=""
+        for key,value in d.items():
+            max_key = max(d, key=d.get)
+            max_value = d[max_key]
+            s=s+(max_key)*max_value
+            d[max_key]=-1
+        return s
+        

53e6eaee22a1575de639b6b92f46faa41789bb90
Kothapelly Sai Sidhartha <115478673+kothapallysidhartha@users.noreply.github.com>
2024-07-08 00:44:38 +0530

    Time: 11 ms (75%), Space: 11.7 MB (24.24%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numWaterBottles(self, numBottles, numExchange):
+        """
+        :type numBottles: int
+        :type numExchange: int
+        :rtype: int
+        """
+        totalBottles = numBottles
+        while numBottles >= numExchange:
+            newBottles = numBottles // numExchange
+            rBottles = numBottles % numExchange
+            totalBottles += newBottles
+            numBottles = newBottles + rBottles
+
+        return totalBottles

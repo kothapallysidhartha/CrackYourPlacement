cc5409d9fb04d05969dc855e6106c6f318d032e5
Kothapelly Sai Sidhartha <115478673+kothapallysidhartha@users.noreply.github.com>
2024-07-21 21:11:44 +0530

    Time: 11 ms (92.75%), Space: 11.5 MB (87.19%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,7 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        d = {}
+
+
+        """d = {}
         for i in nums:
             if i not in d:
                 d[i] = 1
@@ -15,4 +17,19 @@ class Solution(object):
         for key in sorted(d.keys()):
             for _ in range(d[key]):
                 nums[index] = key
-                index += 1
+                index += 1"""
+        start=0
+        mid=0
+        end=len(nums)-1
+        while mid<=end:
+            if nums[mid]==0:
+                nums[start],nums[mid]=nums[mid],nums[start]
+                mid+=1
+                start+=1
+                #end-=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid],nums[end]=nums[end],nums[mid]
+                end-=1
+        

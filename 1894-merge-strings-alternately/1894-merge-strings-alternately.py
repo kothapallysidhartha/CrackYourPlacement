a696f303a634abf9b0bf18bb620435ef26440bf0
Kothapelly Sai Sidhartha <115478673+kothapallysidhartha@users.noreply.github.com>
2024-08-19 10:56:52 +0530

    Time: 10 ms (87.21%), Space: 11.6 MB (57.17%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -8,8 +8,8 @@ class Solution(object):
         k=len(word1) if len(word1)>=len(word2) else len(word2)
         v=""
         for i in range(k):
-            if i in range(len(word1)):
+            if i <(len(word1)):
                 v=v+word1[i]
-            if i in range(len(word2)):
+            if i <(len(word2)):
                 v=v+word2[i]
         return v

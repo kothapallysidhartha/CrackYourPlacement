31a118bbc0eed47cc05e1be1b20087b3d859cc46
Kothapelly Sai Sidhartha <115478673+kothapallysidhartha@users.noreply.github.com>
2025-01-03 12:18:47 +0530

    Time: 14 ms (77.16%), Space: 12.3 MB (31.08%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -4,44 +4,21 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        y = ''
+        # Define 32-bit signed integer range
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
 
-        sign = 1
-        for i in str(x):
-            if i == '-':
-                sign = -1
-            else:
-                y += i
-        y = sign * int(y[::-1])
-        if y < -(2 ** 31) or y >(2**31 - 1):
+        # Check range condition for input
+        if x < INT_MIN or x > INT_MAX:
             return 0
-        return y
-        
-"""import sys
 
-class Solution(object):
-    def reverse(self, x):
-        
-        :type x: int
-        :rtype: int
-    
-        o = 0
-        n = 0
-        
-        if x < 0:
-            n = 1
-            x = -x
-        
-        while x != 0:
-            # Check for overflow before updating o
-            if o > sys.maxsize // 10 or (o == sys.maxsize // 10 and x % 10 > sys.maxsize % 10):
-                return 0  # Return 0 for overflow cases
-            
-            o = (o * 10) + (x % 10)
-            x = x // 10
-        
-        if n == 1:
-            return -o
+        # Reverse integer handling negative cases
+        if x >= 0:
+            rev = int(str(x)[::-1])
         else:
-            return o
-"""
+            rev = -int(str(-x)[::-1])
+
+        # Check if the reversed integer is within the 32-bit signed range
+        if rev < INT_MIN or rev > INT_MAX:
+            return 0
+
+        return rev
